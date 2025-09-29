c552ac1abde58d5833a283d3d7fb477c3b510385
Michael <michaelobineche99@gmail.com>
2025-09-30 00:35:01 +0100

    prisma app/generated ignored from lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,6 +13,7 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     ignores: [
+      "app/generated/**",
       "node_modules/**",
       ".next/**",
       "out/**",
